5c1b4feb39e4134aefd8ee457999eca7234c605f
Nelson <nelsonsuarezj@gmail.com>
2021-12-27 13:48:29 -0500

    instalacion axios

--- src/Components/PrimeReactTable.jsx
+++ src/Components/PrimeReactTable.jsx
@@ -1,4 +1,25 @@
+//import axios from 'axios'
+import { useEffect } from 'react'
+import api from '../controllers/httpClient'
+
 const PrimeReactTable = () => {
+
+    const getRestaurantes = async () => {
+        try {
+            const respuesta = await api.get('/restaurantes')
+            console.log(respuesta.data[0]);
+            console.log('hola');
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
+
+    useEffect(() => {
+        console.log('useEffect listo')
+        getRestaurantes()
+    }, [])
+
     return (
         <div>
             Tabla Prime React
